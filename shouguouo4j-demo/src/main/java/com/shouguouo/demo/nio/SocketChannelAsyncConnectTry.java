f78d9fa34e1654e9341300ebea816fdf8e3e1811
shouguouo <shouweijian@gmail.com>
2022-04-01 18:50:43 +0800

    SocketChannel异步连接，并读取传输的数据

---
+++ shouguouo4j-demo/src/main/java/com/shouguouo/demo/nio/SocketChannelAsyncConnectTry.java
@@ -0,0 +1,47 @@
+package com.shouguouo.demo.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+
+/**
+ * port要有程序监听，可以与ServerSocketChannelTry搭配测试
+ *
+ * @author shouguouo
+ * @date 2022-03-31 21:56:40
+ */
+public class SocketChannelAsyncConnectTry {
+
+    public static void main(String[] argv) throws IOException, InterruptedException {
+        String host = "127.0.0.1";
+        int port = 1234;
+        if (argv.length == 2) {
+            host = argv[0];
+            port = Integer.parseInt(argv[1]);
+        }
+        SocketChannel sc = SocketChannel.open();
+        // 异步连接
+        sc.configureBlocking(false);
+        sc.connect(new InetSocketAddress(host, port));
+        System.out.println("initiating connection");
+        while (!sc.finishConnect()) {
+            doSomethingUseful();
+        }
+        System.out.println("connection established");
+        // Do something with the connected socket
+        // The SocketChannel is still nonblocking
+        System.out.println("isConnected: " + sc.isConnected());
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(24);
+        while (sc.read(byteBuffer) == 0) {
+            // 等待数据读入
+        }
+        System.out.println(Charset.defaultCharset().decode((ByteBuffer) byteBuffer.flip()));
+        sc.close();
+    }
+
+    private static void doSomethingUseful() {
+        System.out.println("doing something useless");
+    }
+}
